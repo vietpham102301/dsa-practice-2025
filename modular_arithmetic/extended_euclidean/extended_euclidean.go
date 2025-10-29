c8faba84ac3e2d1a0160fc2c8b18cb73d6eb34f3
Viet Pham <vietpham102301@gmail.com>
2025-10-29 15:50:57 +0700

    ref: add iterative extended euclid

--- modular_arithmetic/extended_euclidean/extended_euclidean.go
+++ modular_arithmetic/extended_euclidean/extended_euclidean.go
@@ -25,6 +25,21 @@ func extendedGCD(a, b int) (int, int, int) {
 	return gcd, x, y
 }
 
+func extendedGCDIterative(a, b int) (int, int, int) {
+	r0, r1 := a, b // Tổng quát hoá bài toán thành tìm ri sao a*xi + b*yi = ri; với ri = d
+	x0, x1 := 1, 0
+	y0, y1 := 0, 1 // a*x0 + b*y0 = a; a*x1 + b*y1 = b
+
+	for r1 != 0 { // dừng tại bước thứ m-1 khi r(m+2) == 0 tức phép chia a % b là phép chia hết
+		q := r0 / r1 // chia kiểu int trong Go
+		r0, r1 = r1, r0-q*r1
+		x0, x1 = x1, x0-q*x1
+		y0, y1 = y1, y0-q*y1
+	}
+
+	return r0, x0, y0 //g, xi, yi
+}
+
 func main() {
 	a, b := 99, 78
 	g, x, y := extendedGCD(a, b)
