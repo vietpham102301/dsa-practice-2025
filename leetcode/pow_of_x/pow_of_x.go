d70d1cd79a2b9cebedfe2d15264771a8eae372d9
Viet Pham <vietpham102301@gmail.com>
2025-12-09 11:55:34 +0700

    add leetcode 50 pow(x, n)

---
+++ leetcode/pow_of_x/pow_of_x.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+)
+
+func myPow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+
+	neg := n < 0
+	if neg {
+		n = -n
+	}
+
+	res := 1.0
+	for n > 0 {
+		if n&1 == 1 {
+			res *= x
+		}
+		x *= x
+		n >>= 1
+	}
+
+	if neg {
+		return 1 / res
+	}
+
+	return res
+}
+
+func myPowRecursion(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+
+	if n < 0 {
+		return 1 / myPowRecursion(x, -n)
+	}
+
+	if n%2 == 0 {
+		half := myPowRecursion(x, n/2)
+		return half * half
+	}
+
+	return x * myPowRecursion(x, n-1)
+}
+
+func main() {
+	x := 2.0
+	n := 10
+
+	res := myPowRecursion(x, n)
+	fmt.Printf("res: %f", res)
+}
