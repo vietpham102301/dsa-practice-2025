805ecc6f0c55d1c085d65438f43d76179cc542f0
Viet Pham <vietpham102301@gmail.com>
2025-12-26 14:10:25 +0700

    add leetcode 33. search in rotated sorted array

---
+++ leetcode/search_in_rotated_sorted_array/search_in_rotated_sorted_array.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := (left + right) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	nums := []int{5, 1, 2, 3, 4}
+	target := 1
+	res := search(nums, target)
+
+	fmt.Println(res)
+}
