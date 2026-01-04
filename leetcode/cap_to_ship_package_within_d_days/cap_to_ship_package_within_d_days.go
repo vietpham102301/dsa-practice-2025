65deb0aef08c12632d832694438a9cff8f565190
Viet Pham <vietpham102301@gmail.com>
2026-01-04 23:15:49 +0700

    add leetcode 875 & 1011

--- leetcode/cap_to_ship_package_within_d_days/cap_to_ship_package_within_d_days.go
+++ leetcode/cap_to_ship_package_within_d_days/cap_to_ship_package_within_d_days.go
@@ -1,1 +1,60 @@
-package cap_to_ship_package_within_d_days
+package main
+
+import "fmt"
+
+func shipWithinDays(weights []int, days int) int {
+	left, right := findMinCap(weights), findMaxCap(weights)
+	ans := -1
+	for left <= right {
+		mid := (left + right) / 2
+		if canShip(weights, mid, days) {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return ans
+}
+
+func findMaxCap(weights []int) int {
+	s := 0
+	for _, w := range weights {
+		s += w
+	}
+
+	return s
+}
+func findMinCap(weights []int) int {
+	s := 0
+	for _, w := range weights {
+		if w > s {
+			s = w
+		}
+	}
+
+	return s
+}
+
+func canShip(weights []int, cap int, d int) bool {
+	l := 0
+	days := 1
+	for _, w := range weights {
+		l += w
+		if l > cap {
+			days += 1
+			l = w
+		}
+	}
+
+	return days <= d
+}
+
+func main() {
+	weights := []int{1, 2, 3, 1, 1}
+	days := 4
+
+	res := shipWithinDays(weights, days)
+	fmt.Println(res)
+}
--- leetcode/koko_eating_bananas/eat_bananas.go
+++ leetcode/koko_eating_bananas/eat_bananas.go
@@ -1,1 +1,52 @@
-package koko_eating_bananas
+package main
+
+import "fmt"
+
+func minEatingSpeed(piles []int, h int) int {
+	left, right := 1, findMaxSpeed(piles)
+	ans := 1
+	for left <= right {
+		mid := (left + right) / 2
+		t := totalTime(piles, mid)
+		if t <= h {
+			ans = mid
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return ans
+}
+
+func totalTime(piles []int, k int) int {
+	s := 0
+	for _, p := range piles {
+		s += ceil(p, k)
+	}
+
+	return s
+}
+
+func findMaxSpeed(piles []int) int {
+	m := 1
+	for _, p := range piles {
+		if p > m {
+			m = p
+		}
+	}
+
+	return m
+}
+
+func ceil(p, k int) int {
+	return (p + k - 1) / k
+}
+
+func main() {
+	piles := []int{3, 6, 7, 11}
+	h := 8
+	res := minEatingSpeed(piles, h)
+
+	fmt.Println(res)
+}
