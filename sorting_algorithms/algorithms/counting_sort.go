2af657c22874f520a4231f974126dc6ae4a65221
Viet Pham <vietpham102301@gmail.com>
2026-01-25 14:43:10 +0700

    add counting sort

--- sorting_algorithms/algorithms/counting_sort.go
+++ sorting_algorithms/algorithms/counting_sort.go
@@ -5,7 +5,6 @@ func CountingSort(nums []int) []int {
 		return nums
 	}
 
-	// 1. Tìm min và max để xác định phạm vi
 	maxVal, minVal := nums[0], nums[0]
 	for _, num := range nums {
 		if num > maxVal {
@@ -16,21 +15,17 @@ func CountingSort(nums []int) []int {
 		}
 	}
 
-	// 2. Khởi tạo mảng count với kích thước tối ưu
 	k := maxVal - minVal + 1
 	count := make([]int, k)
 
-	// 3. Đếm tần suất (Sử dụng offset là minVal)
 	for _, num := range nums {
 		count[num-minVal]++
 	}
 
-	// 4. Cộng dồn để xác định vị trí
 	for i := 1; i < k; i++ {
 		count[i] += count[i-1]
 	}
 
-	// 5. Xây dựng mảng kết quả (Duyệt ngược để đảm bảo tính ổn định)
 	res := make([]int, len(nums))
 	for i := len(nums) - 1; i >= 0; i-- {
 		num := nums[i]
